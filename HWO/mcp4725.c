c48ac673b9c590aa82fa76a3ac1bfd3d832e0624
BergTim <timon@bergmann.ch>
2019-12-06 11:48:40 +0100

    Initial

---
+++ HWO/mcp4725.c
@@ -0,0 +1,8 @@
+/*
+ * mcp4725.c
+ *
+ *  Created on: 06.12.2019
+ *      Author: timon
+ */
+
+
---
+++ HWO/mcp4725.h
@@ -0,0 +1,13 @@
+/*
+ * mcp4725.h
+ *
+ *  Created on: 06.12.2019
+ *      Author: timon
+ */
+
+#ifndef HWO_MCP4725_H_
+#define HWO_MCP4725_H_
+
+
+
+#endif /* HWO_MCP4725_H_ */
